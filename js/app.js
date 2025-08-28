10f505420a4af21fa4337f02caf2706c6677313c
getSohelRana <getsohelinfo@gmail.com>
2025-08-29 02:53:15 +0600

    call and copy btn js code done

--- js/app.js
+++ js/app.js
@@ -18,5 +18,75 @@ for (const heart  of hearts) {
     })
 }
 
+// counted copy
+let copyCount = 0;
+const showCopyCount = document.getElementById('copy-count');
+
+// coins count
+let mainCoins = 100;
+
+document.getElementById('card-items').addEventListener('click' , (e) => {
+
+    // button action for call-btn
+    if(e.target.className.includes('call-btn')){
+        const callButton = e.target;
+        
+        // coins check for new call
+        if(mainCoins < 20){
+            alert('Not enough coins for the new call');
+            mainCoins = 0;
+            document.getElementById('main-coin').innerText = mainCoins;
+            return
+        } 
+        mainCoins -= 20;
+        document.getElementById('main-coin').innerText = mainCoins;
+
+        // acces call title and number
+        const callTitle = callButton.parentNode.parentNode.children[1].innerText;
+        const callNumber = callButton.parentNode.parentNode.children[3].innerText;
+        
+        // show alert
+        alert(`${callTitle} is ${callNumber}`)
+
+        // Call History Container
+        const callHistoryContainer = document.getElementById('call-history-container');
+        const callHistoryList = document.createElement('div');
+
+        callHistoryList.innerHTML = `
+            <div class="bg-[#FAFAFA] flex md:flex-col lg:flex-row  justify-between md:items-start gap-2 p-2 rounded-xl">
+                <div >
+                    <h1 class="text-lg font-semibold sm:text-[14px] lg:text-[20px] 2xl:font-bold sm:font-normal">${callTitle}</h1>
+                    <p class="xl:font-bold">${callNumber}</p>
+                </div>
+                <p>${new Date().toLocaleTimeString()}</p>
+            </div>
+        `;
+        callHistoryContainer.appendChild(callHistoryList);  
+    }
+    // Clear Call History List
+    document.getElementById('clear-call-history').addEventListener('click' , () => {
+        document.getElementById('call-history-container').innerText = "";
+    })
+
+    //button action for copy-btn
+    if(e.target.className.includes('copy-btn')){
+        const copyBtn = e.target;
+        const copyNumber = copyBtn.parentNode.parentNode.children[3].innerText;
+        // show alert copied text
+        alert(`${copyNumber}`);
+
+        navigator.clipboard.writeText(copyNumber)
+        .then(() => {
+            alert(`Copied: ${copyNumber}`);
+        })
+
+        // count increment
+        copyCount++;
+
+        //show copy count update
+        showCopyCount.innerText = copyCount
+    }
+})
+
 
 
