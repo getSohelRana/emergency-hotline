692a0cacfaea9d91b49b6bfafc491a53da10dc6f
getSohelRana <getsohelinfo@gmail.com>
2025-08-29 01:05:53 +0600

    hearts count js code done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 
+// count hearts
+let heartCount = 0;
+const showhearCount = document.getElementById('heart-count');
+
+const hearts = document.getElementsByClassName('heart');
+for (const heart  of hearts) {
+    heart.addEventListener('click' , () => {
+        if(heart.classList.contains('active')){
+            heart.classList.remove('active' , 'text-green-500');
+            heartCount--;
+        }else{
+            heart.classList.add('active', 'text-green-500');
+             heartCount++;
+        }
+        //show updated heats
+        showhearCount.innerText = heartCount;
+    })
+}
 
 
 
